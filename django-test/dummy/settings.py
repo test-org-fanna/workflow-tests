8ba81e3e9f8d50578e8432b18a499e48cda29d08
fanna <fanna.lautenbach@repowered.nl>
2024-10-29 15:03:18 +0100

    use numbered localhost

--- django-test/dummy/settings.py
+++ django-test/dummy/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         "NAME": os.environ.get("POSTGRES_DB", "postgres"),
         "USER": os.environ.get("POSTGRES_USER", "postgres"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
-        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "HOST": os.environ.get("POSTGRES_HOST", "127.0.0.1"),
         "PORT": os.environ.get("POSTGRES_PORT", 5432),
     }
 }
