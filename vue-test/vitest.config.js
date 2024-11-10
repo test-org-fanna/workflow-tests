fed295e97d41ff233234173a3e989612eedcbbba
fanna <fanna.lautenbach@repowered.nl>
2024-11-10 12:32:55 +0100

    no github action summary + sed i for links in coverage

--- vue-test/vitest.config.js
+++ vue-test/vitest.config.js
@@ -10,7 +10,7 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
-      reporters: [new VitestMarkdownReporter()],
+      reporters: [new VitestMarkdownReporter({ enableGithubActionsSummary: false })],
       outputFile: {
         markdown: "test-report.md",
       },
