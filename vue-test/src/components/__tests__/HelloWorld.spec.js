234ab762f1bd49b6e9a472498101b2ddca03b695
fanna <fanna.lautenbach@repowered.nl>
2024-11-10 16:25:31 +0100

    fix test fails

--- vue-test/src/components/__tests__/HelloWorld.spec.js
+++ vue-test/src/components/__tests__/HelloWorld.spec.js
@@ -6,19 +6,19 @@ import HelloWorld from '../HelloWorld.vue'
 describe('HelloWorld', () => {
   it('renders properly', () => {
     const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('l;afdhjb')
+    expect(wrapper.text()).toContain('Hello Vitest')
   })
 }),
   describe('Math operations', () => {
     it('adds two numbers correctly', () => {
       expect(2 + 3).toBe(5)
       expect(-1 + 1).toBe(0)
-      expect(0 + 0).toBe(1)
+      expect(0 + 0).toBe(0)
     })
 
     it('subtracts two numbers correctly', () => {
       expect(5 - 3).toBe(2)
-      expect(3 - 5).toBe(-3)
+      expect(3 - 5).toBe(-2)
       expect(0 - 0).toBe(0)
     })
   })
