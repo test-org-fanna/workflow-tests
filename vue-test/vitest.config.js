6fbad2052e0550dd213c54e292b48a28a4ab11ea
fanna <fanna.lautenbach@repowered.nl>
2024-11-10 11:33:29 +0100

    coverage report bad; test report good

--- vue-test/vitest.config.js
+++ vue-test/vitest.config.js
@@ -1,6 +1,7 @@
 import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
+import { VitestMarkdownReporter } from "vitest-markdown-reporter";
 
 export default mergeConfig(
   viteConfig,
@@ -9,6 +10,10 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
+      reporters: ["default", new VitestMarkdownReporter()],
+      outputFile: {
+        markdown: "test-report.md",
+      },
     },
   }),
 )
